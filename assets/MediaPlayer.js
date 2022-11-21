f1abde7a0b32f26730adad386d482b0d0bc37363
ifigueroa065 <3059977500302@ingenieria.usac.edu.gt>
2022-11-21 12:39:42 -0600

    first plugin

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,12 +1,22 @@
 function MediaPlayer(config) {
   this.media = config.el;
+  this.plugins= config.plugins || [];
+  this._initPlugins();
 }
+
+MediaPlayer.prototype._initPlugins= function(){
+  this.plugins.forEach(plugin =>{
+    plugin.run(this)
+  })
+}
+
 MediaPlayer.prototype.play = function () {
   this.media.play();
 };
 MediaPlayer.prototype.pause = function () {
   this.media.pause();
 };
+
 MediaPlayer.prototype.togglePlay = function () {
   if (this.media.paused) {
     this.play();
@@ -14,4 +24,14 @@ MediaPlayer.prototype.togglePlay = function () {
     this.pause();
   }
 };
+
+MediaPlayer.prototype.mute= function(){
+  this.media.muted=true;
+}
+
+MediaPlayer.prototype.unmute= function(){
+  this.media.muted=false;
+}
+
+
 export default MediaPlayer;
